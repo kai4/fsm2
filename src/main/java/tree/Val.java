404a6223f4c45ad59ba44085f0fe47b0140beccd
kai4 <869762246@qq.com>
2016-09-07 17:53:51 +0800

    add val

---
+++ src/main/java/tree/Val.java
@@ -0,0 +1,20 @@
+package tree;
+
+public class Val<E> {
+
+	private E val;
+	private E itClass;
+	
+	public E getVal() {
+		return val;
+	}
+	public void setVal(E val) {
+		this.val = val;
+	}
+	public E getItClass() {
+		return itClass;
+	}
+	public void setItClass(E itClass) {
+		this.itClass = itClass;
+	}
+}
