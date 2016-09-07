7f7e6f8ca865b204eaca95defa39bb4d0c9a0376
kai4 <869762246@qq.com>
2016-09-07 21:31:48 +0800

    修改

--- src/main/java/c45/Attribute.java
+++ src/main/java/c45/Attribute.java
@@ -1,5 +1,22 @@
 package c45;
 
-public class Attribute {
+import java.util.HashMap;
+import java.util.Map;
 
+public class Attribute<T, E> {
+	public T name;
+	public Map<Val<E>, Boolean> values = new HashMap<Val<E>, Boolean>();
+	
+	public Attribute(T t) {
+		this.name = t;
+	}
+	
+	public void insertVal(E val, E cls) {
+		Val<E> v = new Val<E>(val, cls);
+		if (values.get(v) == null) {
+			values.put(v, true);
+		} else {
+			values.get(v);
+		}
+	}
 }
--- src/main/java/c45/Val.java
+++ src/main/java/c45/Val.java
@@ -1,5 +1,41 @@
 package c45;
 
-public class Val {
-
+public class Val<E> {
+	public E val;
+	public E cls;
+	public Integer count;
+	
+	public Val(E val, E cls) {
+		this.val = val;
+		this.cls = cls;
+		this.count = 1;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + val.hashCode();
+		result = prime * result + cls.hashCode();
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (this.getClass() != obj.getClass())
+			return false;
+		
+		@SuppressWarnings("rawtypes")
+		Val other = (Val) obj;
+		if (val != other.val)
+			return false;
+		if (cls != other.cls)
+			return false;
+		
+		return true;
+	}
 }
